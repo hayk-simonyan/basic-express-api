cc7e457c87e078e8b89006e4fdfba0e50843c93e
hayk-simonyan <hayksimonyan7@gmail.com>
2022-11-17 18:57:34 +0400

    make the server port optional

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-app.get('/', (req, res) => res.send('Hello world'));
+app.get('/', (req, res) => res.send('Deployed! 🚀'));
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
 console.log(`Server is running on port ${process.env.PORT}`);
